f408719620a078bf56dbab0fec6f78918e7d01b6
Paulo Linhares <paulitolinhares@gmail.com>
2017-11-27 15:53:09 -0300

    Makes sure that the cart has no duplicated items (TODO add quantity count)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,10 +5,12 @@ const defaultState = {
 const gameStoreReducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'ADD_ITEM':
-      return {
-        ...state,
-        cart: [...state.cart, action.item],
-      };
+      return state.cart.find(item => item.id === action.item.id)
+        ? state
+        : {
+          ...state,
+          cart: [...state.cart, action.item],
+        };
     case 'REMOVE_ITEM':
       return {
         ...state,
