84852912079b2c8bd1ec17cc80b925f6c8c2eb47
Paulo Linhares <paulitolinhares@gmail.com>
2017-11-27 13:50:34 -0300

    Starting Cart component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Main from './components/Main';
+import Cart from './components/Cart';
 import './App.css';
 
 const App = () => (
   <div className="container">
     <div className="App">
       <Main />
+      <Cart />
     </div>
   </div>
 );
---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <aside className="Cart">
+        <h3 className="Cart-title">Carrinho</h3>
+      </aside>
+    );
+  }
+}
+
+export default Card;
