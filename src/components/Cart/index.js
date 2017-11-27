b81f2d27aac81d864a931a9e6b382a168b1ea7da
Paulo Linhares <paulitolinhares@gmail.com>
2017-11-27 15:44:04 -0300

    Initial cart items rendering

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { removeItem } from '../../actions';
+import CartItem from '../CartItem';
+import cart from '../../images/cart.svg';
 import './style.css';
 
+const renderItemCounter = itemCount => itemCount === 1 ? '1 item' : `${itemCount} itens`;
+
 class Card extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtotal: 0,
+      shipping: 0,
+      total: 0,
+    };
+
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+
+  handleRemove(id) {
+    const { removeItem, cart } = this.props;
+    const itemToRemove = cart.find(item => item.id === id);
+    removeItem(itemToRemove);
+  }
   render() {
     return (
       <aside className="Cart">
-        <h3 className="Cart-title">Carrinho</h3>
+        <div className="Cart-container">
+        <h3 className="Cart-title">
+          Carrinho
+          {
+            this.props.cart.length > 0 &&
+            <span className="Cart-itemCounter">({renderItemCounter(this.props.cart.length)})</span>
+          }
+        </h3>
+          {
+            this.props.cart.length === 0 &&
+            <div className="Cart-empty">
+              <img src={cart} alt="Carrinho vazio" />
+              <p> Até o momento, <br /> o seu carrinho está vazio </p>
+            </div>
+          }
+          {
+            this.props.cart.length > 0 &&
+            <div className="Cart-content">
+              { this.props.cart.map(item => <CartItem {...item} onRemove={this.handleRemove} />) }
+            </div>
+          }
+        </div>
       </aside>
     );
   }
 }
 
-export default Card;
+const mapStateToProps = state => ({ cart: state.cart });
+
+export default connect(mapStateToProps, { removeItem })(Card);
---
+++ src/components/CartItem/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import formatMoney from '../../lib/formatMoney';
+import './style.css';
+
+
+const CartItem = ({id, name, price, score, image, onRemove}) => {
+  return (
+    <div className="CartItem">
+      <figure className="CartItem-figure">
+        <img className="CartItem-image" src={`images/${image}`}/>
+      </figure>
+      <div className="CartItem-info">
+        <p className="CartItem-name">{name}</p>
+        <p className="CartItem-price">{formatMoney(price)}</p>
+      </div>
+      <button className="CartItem-delete" onClick={() => onRemove(id)}>x</button>
+    </div>
+  );
+};
+
+export default CartItem;
