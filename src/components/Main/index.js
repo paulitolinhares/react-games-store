14cc2a9ba6f8dd97b1d38c07afbdf9e18542f3c5
Paulo Linhares <paulitolinhares@gmail.com>
2017-11-27 13:23:34 -0300

    Refactoring Main to keep the ordering criteria in state

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import GameCard from '../GameCard';
 import data from '../../data/mock.json';
 import './style.css';
 
+const reorderCards = (cards, criteria) => cards.sort((a, b) => a[criteria] >= b[criteria]);
 
-const Main = () => {
-  return (
-    <main className="Main">
-      <nav className="Main-nav">
-        <h1 className="Main-title">Games</h1>
-        <select className="Main-select">
-          <option value="price">Filtrar por preço</option>
-          <option value="score">Filtrar por popularidade</option>
-          <option value="name">Filtrar por ordem alfabética</option>
-        </select>
-      </nav>
-      <div className="Main-cards">
-        { data.map(item => <GameCard {...item} />) }
-      </div>
+class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: reorderCards(data, 'price'),
+    };
+
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  handleSelectChange(e) {
+    const criteria = e.target.value;
+
+    this.setState({
+      cards: reorderCards(this.state.cards, criteria),
+    });
+  }
+  render() {
+    return (
+      <main className="Main">
+        <nav className="Main-nav">
+          <h1 className="Main-title">Games</h1>
+          <select className="Main-select" onChange={this.handleSelectChange}>
+            <option value="price">Filtrar por preço</option>
+            <option value="score">Filtrar por popularidade</option>
+            <option value="name">Filtrar por ordem alfabética</option>
+          </select>
+        </nav>
+        <div className="Main-cards">
+          { this.state.cards.map(item => <GameCard {...item} />) }
+        </div>
     </main>
-  );
-};
+    );
+  }
+}
 
 export default Main;
