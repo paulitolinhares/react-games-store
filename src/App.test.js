03316d28c93e70395e77cae0ce6fb53a86d5738c
Paulo Linhares <paulitolinhares@gmail.com>
2017-11-27 14:49:19 -0300

    Fixes an error on the test caused by the adition of redux

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = createStore(reducer);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, div);
 });
